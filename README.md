c472cab9af61bde86e7a84abb6f03e64f9d65c01
Julissa Bonilla <julissab9@gmail.com>
2023-02-11 13:40:58 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Note Taker Starter Code
+# Note Taker 
+
+## Description
+This weeks challenge was to create an application called Note Taker that could be used to write, delete, and view notes.
+
+## Installation and Usage
+In order to use this application the user will need to install express. This application was created to allow users to be able to create notes with descriptions as well as view their notes previously made. Lastly it allows the user to delete notes that are no longer needed. 
+
+## Test 
+This project can easily be tested if the user run npm run start in the terminal, and clicking the local host link. 
+
+## What I did 
+For this project the front-end was already prepared for us so I just had to work on the back end. For this project the first this i created was a notes.json file and I filled it with some notes to start on the page. After that I began working on my server, I gathered all the required things I needed for my code to work, like express and a PORT. After that I made sure that the app knew that I was used express.json as well as using my public folder. After that I started aquiring my notes using the .get method, then I used the .post method to allow the user to make new notes and making sure that it read to the file and then pushed that new note into the notes.json file I had already created. After I completed that I worked on using the .delete method to allow the user to actually delete notes. 
+
+## Screenshots of my page 
+![alt text](./screenshots/starting.png "Initial Page")
+![alt text](./screenshots/initial.png "View Notes")
+![alt text](./screenshots/delete.png "Deleted note")
+![alt text](./screenshots/update.png "Writing in new note")
+![alt text](./screenshots/newnote.png "New note uploaded")
---
+++ server.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// API Routes
+app.get('/api/notes', (req,res) => {
+    //read notes.json
+    fs.readFile(path.join(__dirname, "./Develop/db/notes.json"), 'utf8', function(err, data) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        const json = JSON.parse(data);
+        res.json(json);
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } =req.body
+
+    if(!title || !text) {
+        res.status(400).json({ error: 'Missing title or text. '})
+        return
+    }
+
+    const newNote = {
+        ...req.body,
+        id: Math.random()
+    }
+
+    //Read contents for adding notes
+    fs.readFile(path.join(__dirname, "./Develop/db/notes.json"), 'utf8', function(err, data) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        const noteData = JSON.parse(data)
+        //push new note into json
+        noteData.push(newNote);
+    //
+    fs.writeFile(path.join(__dirname, "./Develop/db/notes.json"), JSON.stringify(noteData), function (err) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        res.status(200).json(newNote)
+        })
+    })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(!id) {
+        return res.status(400).json({ error: "id is needed" })
+    };
+
+    fs.readFile(path.join(__dirname, "./Develop/db/notes.json"), "utf8", function(err, data) {
+        //parse
+        const noteData = JSON.parse(data)
+        //modify notes
+        const updatedNotes = noteData.filter(notes => id != notes.id)
+        //stringify and save 
+        fs.writeFile(path.join(__dirname, "./Develop/db/notes.json"), JSON.stringify(updatedNotes), function(err) {
+            if (err) {
+                return res.status(500).json(err)
+            }
+            res.json(true)
+        })
+    })
+});
+
+
+//html routes 
+app.get('/', (req, res) => {
+    res.sendFile( path.join(__dirname, "public", "index.html") )
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile( path.join(__dirname, "public", "notes.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
