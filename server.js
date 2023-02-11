2f7dabafb09d9bd37974d440764ddf8bffb066cb
Julissa Bonilla <julissab9@gmail.com>
2023-02-11 14:12:02 -0600

    trying to change port so that it deploys to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3000
+const PORT = 5000
 
 app.use(express.static("public"));
 app.use(express.json());
